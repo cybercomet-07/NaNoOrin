41163f9716ae9862f281e0aabcce120cbf69b72f
Ayush <ayushgadve106@gmail.com>
2026-04-18 23:24:35 +0530

    feat: integrate global ClickSpark kinetic effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import ClickSpark from "@/components/ClickSpark";
+
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
@@ -22,7 +24,17 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} dark h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ClickSpark
+          sparkColor="#C7FF3D"
+          sparkSize={12}
+          sparkRadius={25}
+          sparkCount={10}
+          duration={500}
+        >
+          {children}
+        </ClickSpark>
+      </body>
     </html>
   );
 }
